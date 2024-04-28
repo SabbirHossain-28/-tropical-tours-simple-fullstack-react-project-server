ed96be09ac77bdabbfd3405951535d339073922c
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-04-29 03:43:14 +0600

    create get api for specific id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app=express();
 const port=process.env.PORT || 5000;
@@ -31,6 +31,13 @@ async function run() {
     app.get("/spots", async(req,res)=>{
       const cursor=spotCollection.find();
       const result=await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/spots/:id", async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await spotCollection.findOne(query);
       res.send(result);
     })
 
