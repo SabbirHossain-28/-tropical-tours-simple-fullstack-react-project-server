be78c63b5502a94bb0a7b90b506aafcea40f2558
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-04-28 20:25:05 +0600

    create post api and DB connection for data for spotdata store

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database=client.db("toruismSpotsDB");
+    const spotCollection=database.collection("spots");
+
+    app.post("/spots", async(req,res)=>{
+      const spotsData=req.body;
+      const result=await spotCollection.insertOne(spotsData);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
