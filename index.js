ae8d454a322fac4a94fa3f70efb397ac79f51f61
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-04-29 08:41:01 +0600

    create delete api for specific id

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ async function run() {
       const result=await spotCollection.insertOne(spotsData);
       res.send(result);
     })
+    app.delete("/spots/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const result=await spotCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
